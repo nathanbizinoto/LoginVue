ac6f268a2a573b4e47f44ba826804205cf19c1f3
NATHAN BIZINOTO DE OLIVEIRA <1091392323023@fatec-srv.local>
2024-05-03 21:46:10 -0300

    update vue

--- index.js
+++ index.js
@@ -13,3 +13,53 @@ const LoginComponent = {
         </form>
       </div>
     `,
+    data() {
+        return {
+          credentials: {
+            email: '',
+            password: ''
+          }
+        };
+      },
+      methods: {
+        login() {
+          console.log('Dados de login:', this.credentials);
+        }
+      }
+    };
+    
+    const SigninComponent = {
+      template: `
+        <div class="container">
+          <h1>Cadastro</h1>
+          <form @submit.prevent="register">
+            <label for="email">Email:</label>
+            <input type="email" id="email" v-model="user.email" required>
+    
+            <label for="password">Senha:</label>
+            <input type="password" id="password" v-model="user.password" required>
+    
+            <button type="submit">Cadastrar</button>
+          </form>
+        </div>
+      `,
+      data() {
+        return {
+          user: {
+            email: '',
+            password: ''
+          }
+        };
+      },
+      methods: {
+        register() {
+          console.log('Dados de cadastro:', this.user);
+        }
+      }
+    };
+    
+    const app = Vue.createApp({});
+    app.component('login-component', LoginComponent);
+    app.component('signin-component', SigninComponent);
+    app.mount('#app');
+    
